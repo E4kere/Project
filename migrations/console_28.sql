14d251af15c6efb55da0a2ea21e925db0ed40815
Aidyn <119194293+E4kere@users.noreply.github.com>
2024-05-17 00:54:35 +0500

    Delete migrations directory

--- migrations/console_28.sql
+++
@@ -1,7 +0,0 @@
--- tokens table
-CREATE TABLE tokens (
-                        hash BYTEA PRIMARY KEY,
-                        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-                        expiry TIMESTAMPTZ NOT NULL,
-                        scope VARCHAR(20) NOT NULL
-);
